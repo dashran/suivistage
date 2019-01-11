81d6cc9816229304d943537807ab7b3c149f3a23
dashran <46048613+dashran@users.noreply.github.com>
2019-01-11 16:40:50 +0100

    Delete connexion.php

--- connexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try
-                      {
-                        $connexion = new PDO('mysql:host=localhost:3308;dbname=projetstage;charset=utf8', 'root', '');
-                      }
-                      catch(Exception $e)
-                      {
-                      die('Erreur : '.$e->getMessage());
-                      }?>
